9659679ec290159d2bacde08ef0101325d915177
Or Assayag <orassayag@gmail.com>
2023-04-21 11:46:25 +0300

    ignore eslint errors on deploy

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
