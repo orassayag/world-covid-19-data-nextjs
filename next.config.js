32964eabd67f59ddc20e1d36ab3d4d7c60f2e570
Or Assayag <orassayag@gmail.com>
2023-04-21 11:57:21 +0300

    add vercel compilation bug fix

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
